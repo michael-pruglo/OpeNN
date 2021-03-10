e1ddec8b169a701f4a93d08f64d9c95be2777fd7
m.pruglo <m.pruglo@cleack.com>
2021-03-10 13:39:14 +0200

    refactor core/random

--- Core/random.hpp
+++ Core/random.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "types.hpp"
-#include "utility.hpp"
+#include <Core/types.hpp>
+#include <Core/utility.hpp>
 #include <random>
 #include <ctime>
 
@@ -9,24 +9,33 @@ namespace core
 {
 	inline std::random_device dev;
 	inline std::mt19937 rnd_engine(dev() ^ static_cast<unsigned int>(time(nullptr)));
-
-	inline float_t randd(float_t min = 0.0, float_t max = 1.0)
+	
+	
+	
+	template<typename Floating = float_t>
+    inline
+    typename std::enable_if_t<std::is_floating_point_v<Floating>, Floating>
+	randd(Floating min = 0.0, Floating max = 1.0)
 	{
-		const std::uniform_real_distribution<float_t> distibution(min, max);
+		const std::uniform_real_distribution<Floating> distibution(min, max);
   		return distibution(rnd_engine);
 	}
 
-	inline int randi(int min, int max)
+	template<typename Integral = int>
+    inline
+    typename std::enable_if_t<!std::is_floating_point_v<Integral>, Integral>
+	randi(Integral min, Integral max)
 	{
-		const std::uniform_int_distribution<int> distribution(min, max);
+		const std::uniform_int_distribution<Integral> distribution(min, max);
 		return distribution(rnd_engine);
 	}
 	
-	constexpr float_t W_MIN = -10.0, W_MAX = 10.0;
 
+
+	constexpr float_t _W_MIN = -10.0, _W_MAX = 10.0;
 	inline Vec randVec(size_t n)
 	{
-		return core::generate<Vec>(n, []{ return randd(W_MIN, W_MAX); }); 
+		return core::generate<Vec>(n, []{ return randd(_W_MIN, _W_MAX); }); 
 	}
 
 	inline Matrix randMatrix(size_t n, size_t m)
