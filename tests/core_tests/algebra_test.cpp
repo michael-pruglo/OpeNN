eafb0c6493f85ed3b1d6c408c01db379d4769c23
m.pruglo <m.pruglo@cleack.com>
2021-03-17 19:11:36 +0200

    vec plus test

--- tests/core_tests/algebra_test.cpp
+++ tests/core_tests/algebra_test.cpp
@@ -1,4 +1,5 @@
 #include <tests/common/helpers.hpp>
+#include <core/algebra.hpp>
 
 namespace openn::algebra
 {
@@ -14,6 +15,7 @@ namespace openn::algebra
 
     TEST(CoreAlgebraTest, vec_plus_vec)
     {
-
+        using core::operator+;
+        EXPECT_EQ(core::Vec({ 1., 2., 3. }) + core::Vec({ 6., 17., 8. }), core::Vec({ 7., 19., 11. }));
     }
 }
