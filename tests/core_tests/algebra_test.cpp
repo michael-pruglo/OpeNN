ee48fb1c7b4cba3b1e38572a27355712e1e7c5a2
m.pruglo <m.pruglo@cleack.com>
2021-03-17 18:59:42 +0200

    add algebra test

---
+++ tests/core_tests/algebra_test.cpp
@@ -0,0 +1,19 @@
+#include <tests/common/helpers.hpp>
+
+namespace openn::algebra
+{
+    TEST(CoreAlgebraTest, norm_diff)
+    {
+
+    }
+
+    TEST(CoreAlgebraTest, matrix_mul_vec)
+    {
+
+    }
+
+    TEST(CoreAlgebraTest, vec_plus_vec)
+    {
+
+    }
+}
