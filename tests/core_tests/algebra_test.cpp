7d6a2b25230e9539495fae788195cf3c7d91a5bf
m.pruglo <m.pruglo@cleack.com>
2021-03-18 14:01:33 +0200

    vec + vec test

--- tests/core_tests/algebra_test.cpp
+++ tests/core_tests/algebra_test.cpp
@@ -13,9 +13,33 @@ namespace openn::algebra
 
     }
 
+    core::float_t rand_float_t()
+    {
+        return rand()%9001/1000.0;
+    }
+
     TEST(CoreAlgebraTest, vec_plus_vec)
     {
         using core::operator+;
-        EXPECT_EQ(core::Vec({ 1., 2., 3. }) + core::Vec({ 6., 17., 8. }), core::Vec({ 7., 19., 11. }));
+        constexpr int TEST_CASES_AMOUNT = 1'000, MAX_VEC_LEN = 1'000;
+
+        srand(time(nullptr));
+        for (int test = 0; test < TEST_CASES_AMOUNT; ++test)
+        {
+            core::Vec v1, v2, expected;
+            const int LEN = rand()%MAX_VEC_LEN;
+            v1.reserve(LEN);
+            v2.reserve(LEN);
+            expected.reserve(LEN);
+            for (int i = 0; i < LEN; ++i)
+            {
+                core::float_t a = rand_float_t(), b = rand_float_t();
+                v1.push_back(a);
+                v2.push_back(b);
+                expected.push_back(a+b);
+            }
+
+            expect_double_vec_eq(v1+v2, expected);
+        }
     }
 }
