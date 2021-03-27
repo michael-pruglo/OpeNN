abd8c0bf1140a8be0e73eeb83b220e2371bb417a
m.pruglo <m.pruglo@cleack.com>
2021-03-27 22:25:42 +0200

    resource unpacker tests

--- tests/misc_tests/resource_unpacker_test.cpp
+++ tests/misc_tests/resource_unpacker_test.cpp
@@ -51,17 +51,40 @@ namespace misc::resource_unpacker
 
         EXPECT_EQ(IdxFileUnpacker::unpack(filename), idx_file);
     }
-/*
+
     TEST(MiscResourceUnpackerTest, TwoDimensional)
     {
-        const auto& res = IdxFileUnpacker::unpack("ru_test_2.idx2-ubyte");
-        EXPECT_EQ(res, (std::vector<std::vector<uint8_t>>{}));
+        const std::string filename = PATH_PREF + "ru_test_2.idx2-ubyte";
+        const std::vector<uint8_t> data{ 67U,65U,78U,65U,82U,84U };
+        const IdxFileFormat idx_file{
+            .magic_number = 0x00000802U,
+            .dimensions = { 3u, 2u },
+            .data = data
+        };
+
+        ///create_idx_file(filename, idx_file);
+
+        EXPECT_EQ(IdxFileUnpacker::unpack(filename), idx_file);
     }
 
     TEST(MiscResourceUnpackerTest, ThreeDimensional)
     {
-        const auto& res = IdxFileUnpacker::unpack("ru_test_3.idx3-ubyte");
-        EXPECT_EQ(res, (std::vector<std::vector<std::vector<uint8_t>>>{}));
+        const std::string filename = PATH_PREF + "ru_test_3.idx3-ubyte";
+        const std::vector<uint8_t> data{
+            65u,66u,67u,68u,69u,70u,
+            75u,76u,77u,78u,79u,80u,
+            85u,86u,87u,88u,89u,90u,
+            95u,96u,97u,98u,99u,100u,
+            105u,106u,107u,108u,109u,110u,
+        };
+        const IdxFileFormat idx_file{
+            .magic_number = 0x00000803U,
+            .dimensions = { 5u, 2u, 3u },
+            .data = data
+        };
+
+        //create_idx_file(filename, idx_file);
+
+        EXPECT_EQ(IdxFileUnpacker::unpack(filename), idx_file);
     }
-    */
 }
